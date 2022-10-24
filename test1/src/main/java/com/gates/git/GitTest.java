0e4aeb93508c6f88cdf7398963fc367f5174972d
gates <2280383925@qq.com>
2022-10-24 21:53:51 +0800

    first commit

---
+++ test1/src/main/java/com/gates/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.gates.git;
+
+/**
+ * @ClassName: GitTest
+ * @author: gates
+ * @description:
+ * @version: 1.0.0
+ * @date: 2022/10/24 21:47
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
